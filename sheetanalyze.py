a3f30f00ae694b067f74098c1f311f2a75f95e3f
Azhaan <usazhaan@icloud.com>
2021-01-10 20:11:55 -0600

    main file

--- sheetanalyze.py
+++ sheetanalyze.py
@@ -1,20 +1,26 @@
 inp = input("sheet name: ")
 file = []
 has = 0
-with open(f"{inp}.txt", "r") as f:
+with open(f"sheets/{inp}.txt", "r") as f:
     j = f.readlines()
     for i in j:
         for x in i.split():
             for l in x:
                 if l == '[':
                     has = 1
+                    break
+            x.strip("-")        
             if has == 0 and len(x) > 1:
-                x = ' - '.join(x[i:i + 1] for i in range(0, len(x), 1)) 
+                x = ' - '.join(x[i:i + 1] for i in range(0, len(x), 1))
             x = x.strip("[")
             x = x.strip(']')
             x = x.replace("|", "\n\n")
             file.append(x)
             has = 0
+
+with open(f"sheets/{inp}.txt", "a") as f:
+    f.truncate(0)
+
 with open(f"{inp}.txt", "a") as f:
     for i in range(6):
         f.write("\n")
