8d8dd7fe3ba36524bb9fc3d0d82ce8388fd10d0e
Azhaan <66137324+azh412@users.noreply.github.com>
2021-04-19 10:51:10 -0500

    colors galore

--- VirtualPianoBot.py
+++ VirtualPianoBot.py
@@ -1,55 +1,36 @@
 # Copyright (c) 2021 Azhaan Salam
 # Licensed under the MIT License
 
-import pyautogui, time, random, keyboard
-choice = input("Song name: ")
+from termcolor import colored
+print(colored("sheet name: ", green), end="")
+inp = input()
 
-class Song:
-    def __init__(self, reg, new, dash, name, desc):
-        self.reg = reg # Regular gap between notes (sec)
-        self.new = new # Newline gap between notes (sec)
-        self.dash = dash # Gap for dash separated notes (sec)
-        self.name = name # Name of song
-        self.desc = desc # Real name and artist(s)
+inp = input("sheet name: ")
+file = []
+has = 2
+with open(f"sheets/{inp}.txt", "r") as f:
+    j = f.readlines()
+    for i in j:
+        for x in i.split():
+            for l in x:
+                if l == '[':
+                    has = 1
+                    break
+            x = x.strip("-")        
+            if has == 2 and len(x) > 1:
+                x = ' - '.join(x[i:i + 1] for i in range(0, len(x), 1))
+            x = x.strip("[")
+            x = x.strip(']')
+            x = x.replace("|", "\n\n")
+            file.append(x)
+            has = 2
 
-w = [
-    # All credits to these songs:
-    Song(0.12, 0.5, None, "holiday", "Holiday by Lil Nas X"),
-    Song(0.08, None, None, "pirate", "He's A Pirate by Klaus Badelt"),
-    Song(0.21, None, None, "terimeri", "Teri Meri by Shreya Ghosal and Rafat Fateh Ali Khan"),
-    Song(0.16, 1, 0.05, "christmas", "All I Want For Christmas Is You by Mariah Carey"),
-    Song(0.1, 0.27, None, "astronomia", "Astronomia by Vicetone and Tony Igy"),
-    Song(0.13, None, None, "goldenwind", "Golden Wind from JoJo's Bizzare Adventure"),
-    Song(0.18, 0.03, None, "snowman", "Do You Want To Build A Snowman by Kristen Anderson-Lopez and Robert Lopez"),
-    Song(0.17, 0.25, 0.04, "office", "The Office Theme Song"),
-    Song(0.2, None, 0.03, "avengers", "The Avengers Infinity War Theme"),
-    Song(0.24, None, None, "birthday", "Happy Birthday"),
-    Song(0.18, 0.25, 0.1, "furelise", "Fur Elise by Ludwig Van Beethoven"),
-    Song(0.21, None, None, "unknown", "Into the Unknown by Idina Menzel"),
-    Song(0.22, None, None, "seeyouagain", "See You Again by Wiz Khalifa"),
-    Song(0.13, None, 0.0150, "chariots", "Chariots of Fire by Vangelis"),
-    Song(0.22, None, None, "happier", "Happier by Marshmello")
-]
+with open(f"sheets/{inp}.txt", "w") as f:
+    f.truncate(0)
 
-
-for o in w:
-    if o.name == choice:
-        print(f"Press the Esc key to start playing {o.desc}!")
-        while True:
-            if keyboard.is_pressed("esc"):
-                with open(f"sheets/{o.name}.txt", "r") as text:
-                    f = text.readlines()
-                    for a in f:
-                        if o.new != None:
-                            if a == '\n' == None:
-                                time.sleep(o.new)
-                        for i in a.split():
-                            if o.dash != None:
-                                if i == '-':
-                                    time.sleep(o.dash)
-                                    continue
-                            pyautogui.typewrite(i)
-                            if keyboard.is_pressed("esc"):
-                                exit(0)
-                            time.sleep(o.reg)
-                    exit(0)        
+with open(f"{inp}.txt", "a") as f:
+    for i in range(6):
+        f.write("\n")
+    for i in file:
+        f.write(i)
+        f.write(" ")    
